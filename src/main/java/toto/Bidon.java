d0efd6e17f5fb3dd62ef2e373ae39b84c2cdcb3d
Unknown <gregoiremasse1@hotmail.fr>
2018-11-12 16:11:17 +0100

    oublie main

--- src/main/java/toto/Bidon.java
+++ src/main/java/toto/Bidon.java
@@ -1,6 +1,12 @@
 package toto;
 
 public class Bidon {
+	
+	public String name = "Simon & Garfunkel";
+	
+	public void simonOuGarfunkel{
+		System.out.println("Tu preferes Simon ou Garfunkel ?");
+	}
 
 
 
